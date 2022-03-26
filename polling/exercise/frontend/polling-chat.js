c3392c9d61437880a4efaf12a57f24c8ed71a1f9
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2022-03-25 21:20:59 -0400

    Polling with setTimeout

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -17,11 +17,34 @@ chat.addEventListener("submit", function (e) {
 async function postNewMsg(user, text) {
   // post to /poll a new message
   // write code here
+  const data = {
+    user,
+    text
+  }
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    } 
+  }
+  await fetch("/poll", options);
 }
 
 async function getNewMsgs() {
   // poll the server
   // write code here
+  let myJson;
+  try {
+    const res = await fetch("/poll");
+    myJson = await res.json();
+  }
+  catch(e){
+    console.error("polling error", e)
+  }
+  allChat = myJson.msg;
+  render();
+  setTimeout(getNewMsgs, INTERVAL);
 }
 
 function render() {
