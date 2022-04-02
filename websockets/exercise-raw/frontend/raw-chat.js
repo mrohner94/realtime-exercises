f8d8e3773466f8a4444928299ce926b68a1f4091
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2022-04-01 20:33:23 -0400

    Managing web socket connections

--- websockets/exercise-raw/frontend/raw-chat.js
+++ websockets/exercise-raw/frontend/raw-chat.js
@@ -28,6 +28,10 @@ ws.addEventListener("message", (event) => {
   render();
 })
 
+ws.addEventListener("close", () => {
+  presence.innerText = '🔴'
+})
+
 function render() {
   const html = allChat.map(({ user, text }) => template(user, text));
   msgs.innerHTML = html.join("\n");
